4dbee0dd9cdb783cbd6058676cf3cad3cc0cd898
Vasiliy <shumchenko.94@mail.ru>
2022-11-27 16:04:37 +0300

    add task 4

---
+++ Task 4.py
@@ -0,0 +1,5 @@
+import random
+list = [random.randint(0,10) for i in range(random.randint(5,20))]
+print(f"исходный список:\n {list}")
+random.shuffle(list)
+print(f"список после перемешивания:\n{list}")
